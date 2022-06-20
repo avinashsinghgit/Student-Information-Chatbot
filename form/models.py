d58940956789246a674b6f0b6e4ba54d0b54a1b2
Avinash <as9183181@gmail.com>
2022-06-20 19:05:37 +0530

    Initial Commit

---
+++ form/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+class Info(models.Model):
+    First_Name=models.CharField(max_length=100)
+    Last_Name=models.CharField(max_length=100)
+    Father_Name=models.CharField(max_length=100)
+    Mother_Name=models.CharField(max_length=100)
+    Date_Of_Birth=models.DateField(null=True)
+    gender=models.CharField(max_length=100, null=True)
+    roll=models.CharField(max_length=100)
+    branch=models.CharField(max_length=100, null=True)
+    year=models.CharField(max_length=100, null=True)
+    Contact_Number=models.CharField(max_length=100,null=True)
+    Father_Contact_Number=models.CharField(max_length=100,null=True)
+    Mother_Contact_Number=models.CharField(max_length=100, null=True)
+    Aadhar_Number=models.CharField( max_length=100,null=True)
+    Address=models.TextField(null=True)
+
+
+
+    def __str__(self):
+        return self.First_Name
+    
---
+++ form/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from form.models import Info
+
+# Create your views here.
+
+def index(request):
+
+    try:
+
+        if request.method=="POST":
+            First_Name=request.POST.get("First_Name")
+            Last_Name=request.POST.get("Last_Name")
+            Father_Name=request.POST.get("Father_Name")
+            Mother_Name=request.POST.get("Mother_Name")
+            Date_Of_Birth=request.POST.get("Date_Of_Birth")
+            gender=request.POST.get("gender")
+            roll=request.POST.get("roll")
+            branch=request.POST.get("branch")
+            year=request.POST.get("year")
+            Contact_Number=request.POST.get("Contact_Number")
+            Father_Contact_Number=request.POST.get("Father_Contact_Number")
+            Mother_Contact_Number=request.POST.get("Mother_Contact_Number")
+            Aadhar_Number=request.POST.get("Aadhar_Number")
+            Address=request.POST.get("Address")
+
+            info=Info(First_Name=First_Name, Last_Name=Last_Name, gender=gender, Father_Name=Father_Name, 
+            Mother_Name=Mother_Name, Date_Of_Birth=Date_Of_Birth, roll=roll, branch=branch, year=year, 
+            Contact_Number=Contact_Number, Father_Contact_Number=Father_Contact_Number, Mother_Contact_Number=Mother_Contact_Number,
+            Aadhar_Number=Aadhar_Number,Address=Address)
+
+            info.save()
+    except:
+        return render(request, "error.html")
+
+    return render(request, "index.html")
